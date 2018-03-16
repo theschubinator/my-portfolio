60343dd559b4064b1bc734faeff356e5d1113e85
github username <github email address>
2018-03-15 23:22:29 -0500

    added a pageNotFound component

---
+++ src/components/pageNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const pageNotFound = () => (
+	<div id="page-not-found">
+		<h1>Oops! Unfortunatly, this page does not exist!</h1>
+	</div>
+);
+
+export default pageNotFound;
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from './components/homePage/index';
 import AboutMe from './components/aboutMe/index';
 import Projects from './components/projects/index';
 import Resume from './components/resume';
+import PageNotFound from './components/pageNotFound';
 
 const Routes = () => {
 	return (
 		<Router>
-			<div>
+			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route exact path="/about-me" component={AboutMe} />
 				<Route exact path="/projects" component={Projects} />
 				<Route exact path="/resume" component={Resume} />
-			</div>
+				<Route component={PageNotFound} />
+			</Switch>
 		</Router>
 	);
 }
 
-export default Routes;
+export default Routes;	
